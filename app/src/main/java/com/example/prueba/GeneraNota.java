c72070aa16c81c5a308ee5b350d011a407e3743f
JesusVer <jesusver@ucm.es>
2020-05-24 13:21:02 +0200

    tiriri 2

--- app/src/main/java/com/example/prueba/GeneraNota.java
+++ app/src/main/java/com/example/prueba/GeneraNota.java
@@ -2,9 +2,9 @@ package com.example.prueba;
 
 public class GeneraNota {
 
-    private final int FRECUENCIA_LA = 110;
+    private final int FRECUENCIA_LA = 55;
     private static final GeneraNota ourInstance = new GeneraNota();
-    private final double CONST = Math.pow(2,1/12);
+    private final double CONST = Math.pow(2,(float)1/12);
 
 
     public static GeneraNota getInstance(){
